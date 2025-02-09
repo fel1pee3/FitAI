046ce36d77561c62dcb015946a28b50bfe0d977a
Felipe Maia <132314205+fel1pee3@users.noreply.github.com>
2025-02-09 14:41:03 -0300

    baixando a dieta em psf

--- frontend/src/Routes/Dieta/Dieta.jsx
+++ frontend/src/Routes/Dieta/Dieta.jsx
@@ -4,6 +4,8 @@ import { Link } from 'react-router-dom';
 import DOMPurify from 'dompurify';
 import style from './Dieta.module.css';
 import { FaArrowLeft } from "react-icons/fa6";
+import { FaFilePdf } from "react-icons/fa";
+import { jsPDF } from 'jspdf'; // Importa a biblioteca jsPDF
 
 const Dieta = () => {
 
@@ -27,14 +29,42 @@ const Dieta = () => {
         fetchDiet();
     }, []);
 
+    const downloadPDF = () => {
+        const doc = new jsPDF(); // Cria uma instância do jsPDF
+
+        const dietContent = DOMPurify.sanitize(diet.diet_plan); // Sanitiza o conteúdo
+
+        // Definir o tamanho da fonte para controlar o layout
+        doc.setFontSize(12); // Tamanho da fonte ajustado
+
+        // Adiciona o conteúdo HTML ao PDF
+        doc.html(dietContent, {
+            callback: function (doc) {
+                // Salva o PDF
+                doc.save('dieta.pdf');
+            },
+            margin: [10, 10], // Define margens para ajustar o conteúdo na página
+            x: 10,
+            y: 10,
+            width: 180, // Largura ajustada para caber melhor na página
+            windowWidth: 800, // Ajusta a largura da janela para o conteúdo ser renderizado corretamente
+        });
+    };
+
   return (
     <div className={style.dieta}>
-        <Link to="/HomeStart" className={style.cssButtonsIoButton}>
-        <div className={style.icon}>
-          <FaArrowLeft className={style.arrow} />
+        <div className={style.caixaBtns}>
+            <Link to="/HomeStart" className={style.cssButtonsIoButton}>
+                <div className={style.icon}>
+                <FaArrowLeft className={style.arrow} />
+                </div>
+                Back
+            </Link>
+            <button className={style.downloadPDF} onClick={downloadPDF}>
+                <FaFilePdf className={style.iconPDF}/>
+                <p className={style.text}>Baixar em PDF</p>
+            </button>
         </div>
-        Back
-      </Link>
         <div className={style.container}>
             <div
                 className={style.dietCriada}
